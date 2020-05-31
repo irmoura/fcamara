fa94ed03a7715a5be9095c8903ef780f8a050b3c
Ismael <coragethedogcowardle@gmail.com>
2020-05-31 15:56:27 -0300

    Alterações simples
    Adicionado menu de teste e remoção de trecho em arquivo js

--- js/login/index.js
+++ js/login/index.js
@@ -1,10 +1,3 @@
 $(document).ready(function () {
-    var jqxhr = $.get("https://webhooks.mongodb-stitch.com/api/client/v2.0/app/squadnove-cqynx/service/api/incoming_webhook/usuarios-get?secret=", function(data) {
-        $(data).each(function (i) {
-            
-        });
-    }).done(function() {
 
-    });
-    //alert('Hello');
 });
